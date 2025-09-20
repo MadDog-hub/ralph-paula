84fee48a4d7e9a0b276a72545025d5ac65580cc5
ruel <maddogruel@gmail.com>
2025-09-20 20:53:52 +0800

    add a comment

--- client/src/components/DressCodeSection.tsx
+++ client/src/components/DressCodeSection.tsx
@@ -11,7 +11,7 @@ const DressCodeSection = () => {
       animate={{ opacity: 1 }}
       transition={{ duration: 1, delay: 7.5 }}
     >
-      {/* Background Pattern */}
+      {/* Background Patternn */}
       <div className="absolute inset-0 opacity-5">
         <div className="absolute top-1/3 left-1/4 w-64 h-64 border border-border rounded-full"></div>
         <div className="absolute bottom-1/3 right-1/4 w-48 h-48 border border-border rounded-full"></div>
